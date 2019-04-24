d843f782eb9ba7f2af146153948e9f7f7d41f4c7
Nuno Lopes <nunolopes99@gmail.com>
2019-04-24 21:08:59 +0200

    Fix filename

--- fleet.py
+++ fleet.py
@@ -3,7 +3,7 @@ import random
 from decouple import config
 from time import sleep
 from service import fleet
-from planets import planets, targets
+from coordinates import planets, targets
 
 for alias in sys.argv[2:]:
     sleep(round(random.uniform(4, 6), 1))
